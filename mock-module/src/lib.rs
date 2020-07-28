596cdb8c447a770a41fc75084d29c25a835572c8
xujian <s1473561555@sina.com>
2020-07-28 04:12:40 -0400

    make Addr<Bus> can be saved by Module when Bus started

--- mock-module/src/lib.rs
+++ mock-module/src/lib.rs
@@ -1,10 +1,12 @@
 use actix::prelude::*;
 use ewf_core::message::CallQuery;
-use ewf_core::{error::Error, Call, Event, Module};
+use ewf_core::{error::Error, Bus, Call, Event, Module, StartNotify};
 use serde_json::Value;
 use std::fmt::{self, Debug, Formatter};
 
-pub struct MockModule;
+pub struct MockModule {
+    pub bus_addr: Option<Addr<Bus>>,
+}
 
 impl Actor for MockModule {
     type Context = Context<Self>;
@@ -13,11 +15,13 @@ impl Actor for MockModule {
 impl Handler<Call> for MockModule {
     type Result = ResponseFuture<Result<Value, Error>>;
     fn handle(&mut self, _msg: Call, _ctx: &mut Context<Self>) -> Self::Result {
+        let bus_addr = self.bus_addr.clone().unwrap();
+
         Box::pin(async move {
             let query = CallQuery {
                 module: "mock-module".to_string(),
             };
-            let _caller = _msg.addr.send(query).await;
+            let _caller = bus_addr.send(query).await;
             println!("recv call");
             Ok(Value::default())
         })
@@ -29,6 +33,13 @@ impl Handler<Event> for MockModule {
     fn handle(&mut self, _msg: Event, _ctx: &mut Context<Self>) -> Self::Result {
         println!("recv event");
         Ok(())
+    }
+}
+
+impl Handler<StartNotify> for MockModule {
+    type Result = ();
+    fn handle(&mut self, _msg: StartNotify, _ctx: &mut Context<Self>) -> Self::Result {
+        self.bus_addr = Some(_msg.addr);
     }
 }
 

fe51b223790ef991311ab9445020f20f0047c355
xujian <s1473561555@sina.com>
2020-08-03 05:21:07 -0400

    format the return value of the external interface

--- secret/src/error.rs
+++ secret/src/error.rs
@@ -36,20 +36,35 @@ impl Error {
             Error::DatabaseSelectError => EwfError::OtherError("DatabaseSelectError".to_string()),
             Error::DatabaseInsertError => EwfError::OtherError("DatabaseInsertError".to_string()),
             Error::DatabaseDeleteError => EwfError::OtherError("DatabaseDeleteError".to_string()),
-            Error::DatabaseJsonDeSerializeError => {
-                EwfError::OtherError("DatabaseJsonDeSerializeError".to_string())
-            }
             Error::CallParamValidFaild => EwfError::CallParamValidFaild,
-            Error::RegisterInfoLack => EwfError::OtherError("RegisterInfoLack".to_string()),
-            Error::KeyPairGenError => EwfError::OtherError("KeyPairGenError".to_string()),
-            Error::SecretByidNotFound => EwfError::OtherError("SecretByidNotFound".to_string()),
-            Error::HttpError(err) => EwfError::OtherError(format!("HttpError({})", err)),
             Error::HttpResponseNotJson => EwfError::OtherError("HttpResponseNotJson".to_string()),
+            Error::UnknownSecretType => EwfError::OtherError("UnknownSecretType".to_string()),
             Error::RegisterResponseInvaild => {
                 EwfError::OtherError("RegisterResponseInvaild".to_string())
             }
-            Error::UnknownSecretType => EwfError::OtherError("UnknownSecretType".to_string()),
-            Error::RegisterError(err) => EwfError::OtherError(format!("RegisterError({})", err)),
+            Error::DatabaseJsonDeSerializeError => {
+                EwfError::OtherError("DatabaseJsonDeSerializeError".to_string())
+            }
+            Error::RegisterInfoLack => EwfError::JsonRpcError {
+                code: 1000i64,
+                msg: "请求字段缺失".to_string(),
+            },
+            Error::KeyPairGenError => EwfError::JsonRpcError {
+                code: 1001i64,
+                msg: "密钥对生成错误".to_string(),
+            },
+            Error::SecretByidNotFound => EwfError::JsonRpcError {
+                code: 1002i64,
+                msg: "未发现可用密钥对".to_string(),
+            },
+            Error::HttpError(err) => EwfError::JsonRpcError {
+                code: 1003i64,
+                msg: format!("服务器请求失败: {}", err),
+            },
+            Error::RegisterError(err) => EwfError::JsonRpcError {
+                code: 1004i64,
+                msg: format!("注册失败: ({})", err),
+            },
         }
     }
 }

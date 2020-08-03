fe7d5c07b302e63914e07533fafec0dddd1cda1d
xujian <s1473561555@sina.com>
2020-08-03 05:18:50 -0400

    http request add head X-CLOUD-USER_ID

--- wallet-common/src/http_cli.rs
+++ wallet-common/src/http_cli.rs
@@ -2,10 +2,13 @@ use reqwest::header::{HeaderValue, CONTENT_TYPE};
 use serde_json::{json, Value};
 use std::time::Duration;
 
+const X_CLOUD_USER_ID: &str = "1704a514-1dd2-11b2-802a-557365724164";
+
 pub async fn reqwest_json(url: &str, req: Value, timeout: u64) -> Result<Value, String> {
     match reqwest::Client::new()
         .request(http::Method::POST, url)
         .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
+        .header("X-CLOUD-USER_ID", X_CLOUD_USER_ID)
         .body(req.to_string())
         .timeout(Duration::new(timeout, 0))
         .send()

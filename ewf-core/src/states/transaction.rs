b504fde88be3343406457c34214d61e0ede1c2be
xujian <s1473561555@sina.com>
2020-08-19 22:47:27 -0400

    add state machine transaction define and some macros

--- ewf-core/src/states/transaction.rs
+++ ewf-core/src/states/transaction.rs
@@ -4,12 +4,29 @@ use crate::Machine;
 enum TransactionState {
     Begin,
     Start,
-    Ready,
+    PaymentPlanSend,
+    PaymentPlanRecv,
+    PaymentPlanDone,
+    WaitCurrencyStat,
+    ComputePlan,
+    ExchangeCurrency,
+    SendCurrencyPlan,
+    SendExchangeCurrency,
+    SendCurrencyStat,
+    CurrencyPlanDone,
+    SenderExchangeCurrency,
+    HalfTransaction,
+    EndTransaction,
 }
 
 enum TransactionTransition {
     Starting,
-    TransactionSuccess,
+    PaymentPlanSyn,
+    PaymentPlanAck,
+    RecvPaymentPlanSyn,
+    RecvPaymentPlanAck,
+    IsPayerAndSendCurrencyStat,
+    IsReceiver,
 }
 
 impl From<String> for TransactionTransition {
@@ -17,8 +34,13 @@ impl From<String> for TransactionTransition {
         let t: &str = &t;
         match t {
             "Starting" => TransactionTransition::Starting,
-            "TransactionSuccess" => TransactionTransition::TransactionSuccess,
-            _ => TransactionTransition::Starting,
+            "PaymentPlanSyn" => TransactionTransition::PaymentPlanSyn,
+            "PaymentPlanAck" => TransactionTransition::PaymentPlanAck,
+            "RecvPaymentPlanSyn" => TransactionTransition::RecvPaymentPlanSyn,
+            "RecvPaymentPlanAck" => TransactionTransition::RecvPaymentPlanAck,
+            "IsPayerAndSendCurrencyStat" => TransactionTransition::IsPayerAndSendCurrencyStat,
+            "IsReceiver" => TransactionTransition::IsReceiver,
+            _ => TransactionTransition::PaymentPlanSyn,
         }
     }
 }
@@ -37,14 +59,26 @@ impl Default for TransactionMachine {
 
 impl Machine for TransactionMachine {
     fn name(&self) -> String {
-        "wallet".to_string()
+        "transaction".to_string()
     }
 
     fn to_string(&self) -> String {
         match self.state {
             TransactionState::Begin => "Begin".to_string(),
             TransactionState::Start => "Start".to_string(),
-            TransactionState::Ready => "Ready".to_string(),
+            TransactionState::PaymentPlanSend => "PaymentPlanSend".to_string(),
+            TransactionState::PaymentPlanRecv => "PaymentPlanRecv".to_string(),
+            TransactionState::PaymentPlanDone => "PaymentPlanDone".to_string(),
+            TransactionState::WaitCurrencyStat => "WaitCurrencyStat".to_string(),
+            TransactionState::ComputePlan => "ComputePlan".to_string(),
+            TransactionState::ExchangeCurrency => "ExchangeCurrency".to_string(),
+            TransactionState::SendCurrencyPlan => "SendCurrencyPlan".to_string(),
+            TransactionState::SendExchangeCurrency => "SendExchangeCurrency".to_string(),
+            TransactionState::SendCurrencyStat => "SendCurrencyStat".to_string(),
+            TransactionState::CurrencyPlanDone => "CurrencyPlanDone".to_string(),
+            TransactionState::SenderExchangeCurrency => "SenderExchangeCurrency".to_string(),
+            TransactionState::HalfTransaction => "HalfTransaction".to_string(),
+            TransactionState::EndTransaction => "EndTransaction".to_string(),
         }
     }
 
@@ -55,8 +89,31 @@ impl Machine for TransactionMachine {
                 self.state = TransactionState::Start;
                 Ok(self.to_string())
             }
-            (TransactionState::Start, TransactionTransition::TransactionSuccess) => {
-                self.state = TransactionState::Ready;
+            (TransactionState::Start, TransactionTransition::PaymentPlanSyn) => {
+                self.state = TransactionState::PaymentPlanSend;
+                Ok(self.to_string())
+            }
+            (TransactionState::Start, TransactionTransition::RecvPaymentPlanSyn) => {
+                self.state = TransactionState::PaymentPlanRecv;
+                Ok(self.to_string())
+            }
+            (TransactionState::PaymentPlanSend, TransactionTransition::RecvPaymentPlanAck) => {
+                self.state = TransactionState::PaymentPlanDone;
+                Ok(self.to_string())
+            }
+            (TransactionState::PaymentPlanRecv, TransactionTransition::PaymentPlanAck) => {
+                self.state = TransactionState::PaymentPlanDone;
+                Ok(self.to_string())
+            }
+            (
+                TransactionState::PaymentPlanDone,
+                TransactionTransition::IsPayerAndSendCurrencyStat,
+            ) => {
+                self.state = TransactionState::SendCurrencyPlan;
+                Ok(self.to_string())
+            }
+            (TransactionState::PaymentPlanDone, TransactionTransition::IsReceiver) => {
+                self.state = TransactionState::WaitCurrencyStat;
                 Ok(self.to_string())
             }
             _ => Err(Error::TransitionNotFound),
